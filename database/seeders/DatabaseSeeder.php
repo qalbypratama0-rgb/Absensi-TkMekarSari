49ab919c35acf3fffedd9d81ad5a2f4661f6d125
Muh. Qalby Pratama Rudi <qalbypratama0@gmail.com>
2026-07-05 11:59:36 +0800

    Fix: Mengaktifkan akun admin utama

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -12,11 +12,10 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
-
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        \App\Models\User::create([
+            'name' => 'Admin TK Mekar Sari',
+            'email' => 'admin@gmail.com',
+            'password' => bcrypt('password123'),
+        ]);
     }
 }
